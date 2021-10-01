ee44b8510a9b51ec48ce10b614d83b11fa30cfea
markup54 <marzenakupka@tlen.pl>
2021-10-01 15:30:21 +0200

    Przepis

--- app/src/main/java/com/example/ksiazkakucharska/MainActivity.java
+++ app/src/main/java/com/example/ksiazkakucharska/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ksiazkakucharska;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AdapterView.OnItemClickListener wybrany
+                = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //i który element kliknięto
+
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        ListaPrzepisowActivity.class);
+                startActivity(intent);
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(wybrany);
     }
 }
---
+++ app/src/main/java/com/example/ksiazkakucharska/Przepis.java
@@ -0,0 +1,48 @@
+package com.example.ksiazkakucharska;
+
+public class Przepis {
+    private String nazwa;
+    private int kategoria;
+    private String skladniki;
+    private int idObrazka;
+
+    public Przepis(String nazwa, int kategoria, String skladniki, int idObrazka) {
+        this.nazwa = nazwa;
+        this.kategoria = kategoria;
+        this.skladniki = skladniki;
+        this.idObrazka = idObrazka;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public int getKategoria() {
+        return kategoria;
+    }
+
+    public void setKategoria(int kategoria) {
+        this.kategoria = kategoria;
+    }
+
+    public String getSkladniki() {
+        return skladniki;
+    }
+
+    public void setSkladniki(String skladniki) {
+        this.skladniki = skladniki;
+    }
+
+    public int getIdObrazka() {
+        return idObrazka;
+    }
+
+    public void setIdObrazka(int idObrazka) {
+        this.idObrazka = idObrazka;
+    }
+
+}
